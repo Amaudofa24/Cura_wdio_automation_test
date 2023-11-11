fd112ad1a01c2e1dc55919799fea48d2d72531b4
Amaudofa24 <emmanuel.kuye@wemabank.com>
2023-11-12 00:35:28 +0100

    login_workflow and part appointment_workflow with facility assertion

--- test/pageobjects/appointment.page.js
+++ test/pageobjects/appointment.page.js
@@ -1,25 +1,68 @@
   class AppointmentPage {
 
 
-    get facility () {
+    get facilityDropdown () {
         return $("//select[@id='combo_facility']");
     }
 
-    get hospitalreadmission () {
+    get hospitalReadmission () {
         return $("//input[@id='chk_hospotal_readmission']");
 
-  }
+    }
+
+    get healthCareProgram () {
+        return $('#radio_program_none')
+    }
+
+    get visitDate () {
+        return $('#txt_visit_date')
+    }
+
+    get addComment () {
+        return $('#txt_comment')
+    }
+
+    get bookAppointment () {
+        return $('#btn-book-appointment')
+    }
+
+    get facilitySelected () {
+        return $('#facility')
+    }
+    
 
 
-    async facility() {
-        await this.btnFacility.click();
+    async selectFacility(selectedfacility) {
+        await this.facilityDropdown.selectByVisibleText(selectedfacility);
     }
   
-    async hospitalreadmission() {
-        await this.btnHospitalReadmission.click();
+    async selectReadmission() {
+        await this.hospitalReadmission.click();
     }
 
+    async selectNoneHealthcare() {
+        await this.healthCareProgram.click();
+    }
 
+    async selectVisitDate(date) {
+        await this.visitDate.setValue(date);
+    }
+
+    async insertComment(comment) {
+        await this.addComment.setValue(comment);
+    }
+
+    async makeAppointment() {
+        await this.bookAppointment.click();
+    }
+
+    async bookedFacility() {
+        await this.facilitySelected.getText();
+
+    
+  
+  
+    }
 }
 
 
--- test/specs/appointment.spec.js
+++ test/specs/appointment.spec.js
@@ -3,7 +3,11 @@ const AppointmentPage = require('../pageobjects/appointment.page')
 
 describe ('Appointment Workflow', function () {
      const valid_username = 'John Doe'
-     const valid_password = 'ThisIsNotAPassword' 
+     const valid_password = 'ThisIsNotAPassword'
+     const tokyo_facility = 'Tokyo CURA Healthcare Center'
+     const Date = '12/11/2023'
+     const feedback = 'Malaria'
+     
 
 
   it('should book an appointment', async () => {
@@ -11,17 +15,24 @@ describe ('Appointment Workflow', function () {
         await LoginPage.login(valid_username, valid_password) 
         var url = await LoginPage.currentUrl();
         expect(url).toContain('#appointment')
-        const dropDown = await $("//select[@id='combo_facility']");
-        await dropDown.selectByIndex(1);
-       // $("//select[@id='combo_facility']").selectByIndex(1);
-        console.log(await dropDown.getValue());
-        let element = await $("//input[@id='chk_hospotal_readmission']");
-        console.log(await element.isSelected());
-        
+        await AppointmentPage.selectFacility(tokyo_facility);
+        await AppointmentPage.selectReadmission();        
+        await AppointmentPage.selectNoneHealthcare();
+        await AppointmentPage.selectVisitDate(Date);
+        await AppointmentPage.insertComment(feedback);
+        await AppointmentPage.makeAppointment();
+        const facility = await AppointmentPage.bookedFacility();
+        expect(facility).toHaveText('Tokyo CURA Healthcare Center');
+        // await AppointmentPage.assertfacilityDropdown();
+        // await AppointmentPage.asserthospitalReadmission('yes');
+        // await AppointmentPage.asserthealthCareProgram('None');
+        // await AppointmentPage.assertvisitDate('12/11/2023');
+        // await AppointmentPage.assertinsertComment('Malaria')
+         
 
 
 
-  });
+    });
 
 
 
