059c194019b111d0e402434133789b91a37bc582
Amaudofa24 <emmanuel.kuye@wemabank.com>
2023-11-12 03:28:38 +0100

    login_workflow and part appointment_workflow with facility assertions

--- test/pageobjects/appointment.page.js
+++ test/pageobjects/appointment.page.js
@@ -5,14 +5,23 @@
         return $("//select[@id='combo_facility']");
     }
 
+
     get hospitalReadmission () {
         return $("//input[@id='chk_hospotal_readmission']");
 
     }
 
-    get healthCareProgram () {
+    get noneCareProgram () {
         return $('#radio_program_none')
     }
+
+    get medicareCareProgram () { 
+        return $('#radio_program_medicare')
+    }
+
+    // get medicaidCareProgram () {
+    //     return $('#radio_program_medicaid')
+    // }
 
     get visitDate () {
         return $('#txt_visit_date')
@@ -29,6 +38,26 @@
     get facilitySelected () {
         return $('#facility')
     }
+
+    get dateSelected () {
+        return $('#visit_date')
+    }
+
+    get commentWritten () {
+        return $('#comment')
+    }
+
+    get pickedReadmission () {
+        return $('#hospital_readmission')
+    }
+
+    get pickedProgram () {
+        return $('#program')
+    }
+
+    get homePage () {
+        return $("//a[normalize-space()='Go to Homepage']")
+    }
     
 
 
@@ -41,8 +70,16 @@
     }
 
     async selectNoneHealthcare() {
-        await this.healthCareProgram.click();
+        await this.noneCareProgram.click();
     }
+
+    async selectMedicareHealthcare() {
+        await this.medicareCareProgram.click(); 
+    }
+
+    // async selectMedicaidHealthcare() {
+    //     await this.medicaidCareProgram.click(); 
+    // }
 
     async selectVisitDate(date) {
         await this.visitDate.setValue(date);
@@ -56,13 +93,46 @@
         await this.bookAppointment.click();
     }
 
-    async bookedFacility() {
+    async bookedTokyoFacility() {
         await this.facilitySelected.getText();
-
-    
-  
-  
     }
+
+
+    async bookedDate() {
+        await this.dateSelected.getText();
+    }
+
+    async comment () {
+        await this.commentWritten.getText();
+    }
+
+     async readmissionSelected() {
+        await this.pickedReadmission.getText();
+    }
+
+    async selectedProgram() {
+        await this.pickedProgram.getText();
+    }
+
+      async bookedHongKongFacility() {
+        await this.facilitySelected.getText();
+    }
+
+   
+
+
+    //   async bookedTokyoFacility() {
+    //     await this.facilitySelected.getText();
+    // }
+
+
+
+    async btnHomePage() {
+        await this.homePage.click();
+    }
+  
+  
+    
 }
 
 
--- test/specs/appointment.spec.js
+++ test/specs/appointment.spec.js
@@ -5,8 +5,11 @@ describe ('Appointment Workflow', function () {
      const valid_username = 'John Doe'
      const valid_password = 'ThisIsNotAPassword'
      const tokyo_facility = 'Tokyo CURA Healthcare Center'
+     const hongkong_facility = 'HongKong CURA Healthcare Center'
      const Date = '12/11/2023'
+     const hongkong_date = '24/11/2023'
      const feedback = 'Malaria'
+     const hongkong_feedback = 'Headache'
      
 
 
@@ -21,15 +24,35 @@ describe ('Appointment Workflow', function () {
         await AppointmentPage.selectVisitDate(Date);
         await AppointmentPage.insertComment(feedback);
         await AppointmentPage.makeAppointment();
-        const facility = await AppointmentPage.bookedFacility();
+        const facility = await AppointmentPage.bookedTokyoFacility();
         expect(facility).toHaveText('Tokyo CURA Healthcare Center');
-        
-         
-
+        const appointmentdate = await AppointmentPage.bookedDate();
+        expect(appointmentdate).toHaveText('12/11/2023');
+        const comments = await AppointmentPage.comment();
+        expect(comments).toHaveText('Malaria');
+        const readmin = await AppointmentPage.readmissionSelected();
+        expect(readmin).toHaveText('Yes');
+        const program = await AppointmentPage.selectedProgram();
+        expect(program).toHaveText('None');
+        await AppointmentPage.btnHomePage();
 
 
     });
 
+    // it('should assert the appointment details', async () => {
+    //      await LoginPage.open();
+    //      await AppointmentPage.selectFacility(hongkong_facility);
+    //      await AppointmentPage.selectReadmission();
+    //      await AppointmentPage.selectMedicareHealthcare();
+    //      await AppointmentPage.selectVisitDate(hongkong_date);
+    //      await AppointmentPage.insertComment(hongkong_feedback);
+    //      await AppointmentPage.makeAppointment();
+    //      const hongkong = await AppointmentPage.bookedHongKongFacility();
+    //     expect(hongkong).toHaveText('HongKong CURA Healthcare Center');
+    //     await AppointmentPage.btnHomePage();      
+
+
+    // })
 
 
 
